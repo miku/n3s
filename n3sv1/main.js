b576f19f354a12e5c0f401af3147d8b5fc7d41e2
Martin Czygan <martin.czygan@gmail.com>
2024-12-14 03:00:08 +0100

    try

--- n3sv1/main.js
+++ n3sv1/main.js
@@ -11,6 +11,8 @@ const createWindow = () => {
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
+                      contextIsolation: true,
+                        enableRemoteModule: false,
         }
     })
     win.loadFile('index.html')
--- n3sv1/preload.js
+++ n3sv1/preload.js
@@ -1,5 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
-const path = require('node:path'); // not available, here
+const path = require('path'); // not available, here
 
 contextBridge.exposeInMainWorld('api', {
         getCurrentDirectory: () => process.cwd()
