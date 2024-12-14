8cf018ad2ad507c0f65c2f6eb5202d5c76ec6571
Martin Czygan <martin.czygan@gmail.com>
2024-12-14 02:39:40 +0100

    try to expose the date

---
+++ n3sv1/renderer.js
@@ -0,0 +1,2 @@
+const area = document.getElementById('note-textarea');
+area.value = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`
